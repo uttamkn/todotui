c852db981ea6ad87f8eed21c74c6412acbc78008
uttamkn <uttamkn15@gmail.com>
2024-08-10 22:36:20 +0530

    Add makefile

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,35 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 class Task {
   int id;
   std::string desc;
 
 public:
   Task(int id, std::string desc) : id(id), desc(desc) {}
-
-public:
-  int getId() { return id; }
-  std::string getTask() { return desc; }
+  int getTaskId() const { return id; }
+  std::string getTaskDesc() const { return desc; }
+  std::string getTask() const {
+    return std::to_string(getTaskId()).append(" ").append(getTaskDesc());
+  }
 };
 
 class ManageTasks {
-  // TODO: try using a map instead of a vector
+  // TODO: Use hashmap instead of a vector
   std::vector<Task> tasks;
-  int id;
-
-  void addTask(std::string desc) {
-    Task task(id++, desc);
-    tasks.push_back(task);
-    std::cout << "\n\nTask added\n\n\n";
-    // TODO: Save all tasks in a file
-  }
-
-  void deleteTask(int id) {
-    int i = 0;
-    for (Task task : tasks) {
-      if (task.getId() == id) {
-        tasks.erase(tasks.begin() + i);
-        std::cout << "\n\nTask deleted\n\n\n";
-        return;
-      }
-      i++;
-    }
-    std::cout << "\n\nTask not found\n\n\n";
-  }
-
-  void displayTasks() {
-    std::cout
-        << "=========================== TODOs ==============================\n";
-    if (tasks.size() == 0) {
-      std::cout << "\nNone.\n\n";
-      std::cout << "==========================================================="
-                   "=====\n\n";
-      return;
-    }
-
-    int idx = 1;
-    for (Task task : tasks) {
-      std::cout << idx << ". " << task.getTask() << "(id: " << task.getId()
-                << ")"
-                << "\n";
-      idx++;
-    }
-
-    std::cout << "============================================================="
-                 "===\n\n";
-  }
+  static int id;
 
 public:
   // Constructor
-  ManageTasks() : id(0) {
-    // TODO: import all tasks from the saved file
-  }
+  // TODO: Save all tasks in a file
+  ManageTasks() {}
 
-  // User interface
-  void ui() {
+  // User Interface for the console
+  void console_ui() {
     using namespace std;
 
     while (true) {
-      // TODO: Add update functionality
       displayTasks();
       cout << "Options: a - add a task\t\td - mark a task as completed\t\tq - "
               "quit\n";
@@ -86,19 +44,19 @@ public:
       } break;
 
       case 'd': {
-        string id_str;
-        int id;
+        string input_id_str;
+        int input_id;
         cout << "Enter the task id u want to delete: ";
-        getline(cin, id_str);
+        getline(cin, input_id_str);
 
         try {
-          id = stoi(id_str);
+          input_id = stoi(input_id_str);
         } catch (exception &ex) {
           cout << "\n\nInvalid id\n\n\n";
           continue;
         }
 
-        deleteTask(id);
+        deleteTask(input_id);
       } break;
 
       case 'q': {
@@ -113,10 +71,58 @@ public:
       }
     }
   }
+
+private:
+  void addTask(std::string desc) {
+    Task task(id++, desc);
+    tasks.push_back(task);
+    // addTaskToFile()
+    std::cout << "\n\nTask added\n\n\n";
+  }
+
+  void deleteTask(int id) {
+    int i = 0;
+    for (Task task : tasks) {
+      if (task.getTaskId() == id) {
+        tasks.erase(tasks.begin() + i);
+        // deleteTaskFromFile()
+        std::cout << "\n\nTask deleted\n\n\n";
+        return;
+      }
+      i++;
+    }
+    std::cout << "\n\nTask not found\n\n\n";
+  }
+
+  // TODO: Add update functionality
+
+  void displayTasks() {
+    std::cout
+        << "=========================== TODOs ==============================\n";
+    if (tasks.size() == 0) {
+      std::cout << "\nNone.\n\n";
+      std::cout << "==========================================================="
+                   "=====\n\n";
+      return;
+    }
+
+    int idx = 1;
+    for (Task task : tasks) {
+      std::cout << idx << ". " << task.getTaskDesc()
+                << "(id: " << task.getTaskId() << ")"
+                << "\n";
+      idx++;
+    }
+
+    std::cout << "============================================================="
+                 "===\n\n";
+  }
 };
+
+int ManageTasks::id = 0;
 
 int main() {
   ManageTasks myTasks;
-  myTasks.ui();
+  myTasks.console_ui();
   return 0;
 }
